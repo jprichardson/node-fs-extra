824f7271e96e235a2c97cb883fdea601982bb598
JP Richardson <jprichardson@gmail.com>
2013-10-07 09:20:07 -0500

    reformatted copy() tests

--- test/copy.test.js
+++ test/copy.test.js
@@ -32,53 +32,56 @@ describe('fs-extra', function() {
   */
 
   describe('+ copy()', function() {
-      
-    it('should copy the file asynchronously', function(done) {
-      var fileSrc = path.join(DIR, "TEST_fs-extra_src")
-        , fileDest = path.join(DIR, "TEST_fs-extra_copy")
-        , fileSrc = testutil.createFileWithData(fileSrc, SIZE)
-        , srcMd5 = crypto.createHash('md5').update(fs.readFileSync(fileSrc)).digest("hex")
-        , destMd5 = '';
-      
-      fs.copy(fileSrc, fileDest, function(err) {
-        destMd5 = crypto.createHash('md5').update(fs.readFileSync(fileDest)).digest("hex");
-        T (srcMd5 === destMd5);
-        done()
-      })
-    })
-
-    it('should copy the directory asynchronously', function(done) {
-      var FILES = 2
-        , src = path.join(DIR, 'src')
-        , dest = path.join(DIR, 'dest');
+    describe('> when the source is a file', function() {
+      it('should copy the file asynchronously', function(done) {
+        var fileSrc = path.join(DIR, "TEST_fs-extra_src")
+          , fileDest = path.join(DIR, "TEST_fs-extra_copy")
+          , fileSrc = testutil.createFileWithData(fileSrc, SIZE)
+          , srcMd5 = crypto.createHash('md5').update(fs.readFileSync(fileSrc)).digest("hex")
+          , destMd5 = '';
         
-      mkdir(src, function(err) {
-        for (var i = 0; i < FILES; ++i) 
-          testutil.createFileWithData(path.join(src, i.toString()), SIZE);
-        
-        var subdir = path.join(src, 'subdir');
-        mkdir(subdir, function(err) {
-          for (var i = 0; i < FILES; ++i) 
-            testutil.createFileWithData(path.join(subdir, i.toString()), SIZE);
-          
-          fs.copy(src, dest, function(err) {
-            F (err);
-            T (fs.existsSync(dest));
-            
-            for (var i = 0; i < FILES; ++i) 
-              T (fs.existsSync(path.join(dest, i.toString())));
-            
-            
-            var destSub = path.join(dest, 'subdir');
-            for (var j = 0; j < FILES; ++j)
-              T (fs.existsSync(path.join(destSub, j.toString())));
-            
-            done();
-          })
+        fs.copy(fileSrc, fileDest, function(err) {
+          destMd5 = crypto.createHash('md5').update(fs.readFileSync(fileDest)).digest("hex");
+          T (srcMd5 === destMd5);
+          done()
         })
       })
     })
 
+
+    describe('> when the source is a directory', function() {
+      it('should copy the directory asynchronously', function(done) {
+        var FILES = 2
+          , src = path.join(DIR, 'src')
+          , dest = path.join(DIR, 'dest');
+          
+        mkdir(src, function(err) {
+          for (var i = 0; i < FILES; ++i) 
+            testutil.createFileWithData(path.join(src, i.toString()), SIZE);
+          
+          var subdir = path.join(src, 'subdir');
+          mkdir(subdir, function(err) {
+            for (var i = 0; i < FILES; ++i) 
+              testutil.createFileWithData(path.join(subdir, i.toString()), SIZE);
+            
+            fs.copy(src, dest, function(err) {
+              F (err);
+              T (fs.existsSync(dest));
+              
+              for (var i = 0; i < FILES; ++i) 
+                T (fs.existsSync(path.join(dest, i.toString())));
+              
+              
+              var destSub = path.join(dest, 'subdir');
+              for (var j = 0; j < FILES; ++j)
+                T (fs.existsSync(path.join(destSub, j.toString())));
+              
+              done()
+            })
+          })
+        })
+      })
+    })
   })
 })
 
