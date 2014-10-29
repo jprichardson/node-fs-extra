babf14c7ca1859f41c2d0b8707a99a665f2c8ae9
JP Richardson <jprichardson@gmail.com>
2014-10-29 14:02:19 -0500

    test/mkdir.test: removed semicolons

--- test/mkdir.test.js
+++ test/mkdir.test.js
@@ -1,6 +1,6 @@
 var fs = require('../lib')
   , path = require('path-extra')
-  , testutil = require('testutil');
+  , testutil = require('testutil')
 
 var terst = require('terst')
 
@@ -13,53 +13,53 @@ describe('fs-extra', function() {
 
   describe('+ mkdirs()', function() {
     it('should make the directory', function(done) {
-      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
       
-      F (fs.existsSync(dir));
+      F (fs.existsSync(dir))
       
       fs.mkdirs(dir, function(err) {
-        T (err === null);
-        T (fs.existsSync(dir));
+        T (err === null)
+        T (fs.existsSync(dir))
         
-        done();
+        done()
       })
     })
     
     it('should make the entire directory path', function(done) {
       var dir = path.join(path.tempdir(), 'tmp-' + Date.now() + Math.random())
-        , newDir = path.join(TEST_DIR, 'dfdf', 'ffff', 'aaa');
+        , newDir = path.join(TEST_DIR, 'dfdf', 'ffff', 'aaa')
       
-      F (fs.existsSync(dir));
+      F (fs.existsSync(dir))
       
       fs.mkdirs(newDir, function(err) {
-        T (err === null);
-        T (fs.existsSync(newDir));
+        T (err === null)
+        T (fs.existsSync(newDir))
         
-        done();
-      });
+        done()
+      })
     })
   })
   
   describe('+ mkdirsSync()', function() {
     it('should make the directory', function(done) {
-      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
       
-      F (fs.existsSync(dir));
-      fs.mkdirsSync(dir);
-      T (fs.existsSync(dir));
+      F (fs.existsSync(dir))
+      fs.mkdirsSync(dir)
+      T (fs.existsSync(dir))
       
-      done();
+      done()
     })
 
     it('should make the entire directory path', function(done) {
       var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
-        , newDir = path.join(dir, 'dfdf', 'ffff', 'aaa');
+        , newDir = path.join(dir, 'dfdf', 'ffff', 'aaa')
       
-      F (fs.existsSync(dir));
-      fs.mkdirsSync(dir);
-      T (fs.existsSync(dir));
+      F (fs.existsSync(dir))
+      fs.mkdirsSync(dir)
+      T (fs.existsSync(dir))
       
-      done();
+      done()
     })
   })
 
