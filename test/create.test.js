1dc030be70f0a4cee67998d065308edaf1ca5d3f
JP Richardson <jprichardson@gmail.com>
2014-11-01 21:20:33 -0500

    test/create.test: remove semicolons

--- test/create.test.js
+++ test/create.test.js
@@ -4,35 +4,35 @@ var testutil = require('testutil')
 
 var terst = require('terst')
 
-var TEST_DIR = '';
+var TEST_DIR = ''
 
 describe('fs-extra', function () {
   beforeEach(function() {
-    TEST_DIR = testutil.createTestDir('fs-extra');
+    TEST_DIR = testutil.createTestDir('fs-extra')
   })
 
   describe('+ createFile', function() {
     describe('> when the file and directory does not exist', function() {
       it('should create the file', function(done) {
-        var file = path.join(TEST_DIR, Math.random() + 't-ne', Math.random() + '.txt');
-        F (fs.existsSync(file));
+        var file = path.join(TEST_DIR, Math.random() + 't-ne', Math.random() + '.txt')
+        F (fs.existsSync(file))
         fs.createFile(file, function(err) {
-          F (err);
-          T (fs.existsSync(file));
-          done();
+          F (err)
+          T (fs.existsSync(file))
+          done()
         })
       })
     })
 
     describe('> when the file does exist', function() {
       it('should not modify the file', function(done) {
-        var file = path.join(TEST_DIR, Math.random() + 't-e', Math.random() + '.txt');
+        var file = path.join(TEST_DIR, Math.random() + 't-e', Math.random() + '.txt')
         fs.mkdirsSync(path.dirname(file))
-        fs.writeFileSync(file, 'hello world');
+        fs.writeFileSync(file, 'hello world')
         fs.createFile(file, function(err) {
-          F (err);
-          T (fs.readFileSync(file, 'utf8') === 'hello world');
-          done();
+          F (err)
+          T (fs.readFileSync(file, 'utf8') === 'hello world')
+          done()
         })
       })
     })
@@ -41,22 +41,21 @@ describe('fs-extra', function () {
   describe('+ createFileSync', function() {
     describe('> when the file and directory does not exist', function() {
       it('should create the file', function() {
-        var file = path.join(TEST_DIR, Math.random() + 'ts-ne', Math.random() + '.txt');
-        F (fs.existsSync(file));
-        fs.createFileSync(file);
-        T (fs.existsSync(file));
+        var file = path.join(TEST_DIR, Math.random() + 'ts-ne', Math.random() + '.txt')
+        F (fs.existsSync(file))
+        fs.createFileSync(file)
+        T (fs.existsSync(file))
       })
     })
 
     describe('> when the file does exist', function() {
       it('should not modify the file', function() {
-        var file = path.join(TEST_DIR, Math.random() + 'ts-e', Math.random() + '.txt');
+        var file = path.join(TEST_DIR, Math.random() + 'ts-e', Math.random() + '.txt')
         fs.mkdirsSync(path.dirname(file))
-        fs.writeFileSync(file, 'hello world');
-        fs.createFileSync(file);
-        T (fs.readFileSync(file, 'utf8') === 'hello world');
+        fs.writeFileSync(file, 'hello world')
+        fs.createFileSync(file)
+        T (fs.readFileSync(file, 'utf8') === 'hello world')
       })
     })
   })
-  
 })
