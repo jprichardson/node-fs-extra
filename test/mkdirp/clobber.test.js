e28b10189ddc47ff5104ca2c061aa1ff5503a208
JP Richardson <jprichardson@gmail.com>
2014-11-10 12:53:40 -0600

    test/mkdirp/clobber.test: changed from tap to assert and bdd

--- test/mkdirp/clobber.test.js
+++ test/mkdirp/clobber.test.js
@@ -1,37 +1,41 @@
-var mkdirp = require('../').mkdirp
-var path = require('path')
+var assert = require('assert')
 var fs = require('fs')
-var test = require('tap').test
+var path = require('path')
+var fse = require('../../')
+var testutil = require('testutil')
 
-var ps = [ '', 'tmp' ]
+describe('mkdirp / clobber', function() {
+  var ps, file, itw
 
-for (var i = 0; i < 25; i++) {
-  var dir = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
-  ps.push(dir)
-}
+  before(function(done) {
+    ps = [ '', testutil.createTestDir('fs-extra')]
 
-var file = ps.join('/')
+    for (var i = 0; i < 25; i++) {
+      var dir = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
+      ps.push(dir)
+    }
 
-// a file in the way
-var itw = ps.slice(0, 3).join('/')
+    file = ps.join('/')
 
+    // a file in the way
+    itw = ps.slice(0, 3).join('/')
 
-test('clobber-pre', function (t) {
-  console.error("about to write to "+itw)
-  fs.writeFileSync(itw, 'I AM IN THE WAY, THE TRUTH, AND THE LIGHT.')
+    console.error("about to write to "+itw)
+    fs.writeFileSync(itw, 'I AM IN THE WAY, THE TRUTH, AND THE LIGHT.')
 
-  fs.stat(itw, function (er, stat) {
-    t.ifError(er)
-    t.ok(stat && stat.isFile(), 'should be file')
-    t.end()
+    fs.stat(itw, function (er, stat) {
+      assert.ifError(er)
+      assert.ok(stat && stat.isFile(), 'should be file')
+      done()
+    })
+  })
+
+  it('should clobber', function (done) {
+    fse.mkdirp(file, 0755, function (err) {
+      assert.ok(err)
+      assert.equal(err.code, 'ENOTDIR')
+      done()
+    })
   })
 })
 
-test('clobber', function (t) {
-  t.plan(2)
-  mkdirp(file, 0755, function (err) {
-    t.ok(err)
-    t.equal(err.code, 'ENOTDIR')
-    t.end()
-  })
-})
