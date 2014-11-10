70aa855a4f90f2c8ce422477ff2281d05aad3b62
JP Richardson <jprichardson@gmail.com>
2014-11-10 12:32:32 -0600

    test/mkdirp/chmod.test: removed semicolons

--- test/mkdirp/chmod.test.js
+++ test/mkdirp/chmod.test.js
@@ -1,38 +1,38 @@
-var mkdirp = require('../').mkdirp;
-var path = require('path');
-var fs = require('fs');
-var test = require('tap').test;
+var mkdirp = require('../').mkdirp
+var path = require('path')
+var fs = require('fs')
+var test = require('tap').test
 
-var ps = [ '', 'tmp' ];
+var ps = [ '', 'tmp' ]
 
-for (var i = 0; i < 25; i++) {
-    var dir = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
-    ps.push(dir);
+for (var i = 0 i < 25 i++) {
+    var dir = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
+    ps.push(dir)
 }
 
-var file = ps.join('/');
+var file = ps.join('/')
 
 test('chmod-pre', function (t) {
     var mode = 0744
     mkdirp(file, mode, function (er) {
-        t.ifError(er, 'should not error');
+        t.ifError(er, 'should not error')
         fs.stat(file, function (er, stat) {
-            t.ifError(er, 'should exist');
-            t.ok(stat && stat.isDirectory(), 'should be directory');
-            t.equal(stat && stat.mode & 0777, mode, 'should be 0744');
-            t.end();
-        });
-    });
-});
+            t.ifError(er, 'should exist')
+            t.ok(stat && stat.isDirectory(), 'should be directory')
+            t.equal(stat && stat.mode & 0777, mode, 'should be 0744')
+            t.end()
+        })
+    })
+})
 
 test('chmod', function (t) {
     var mode = 0755
     mkdirp(file, mode, function (er) {
-        t.ifError(er, 'should not error');
+        t.ifError(er, 'should not error')
         fs.stat(file, function (er, stat) {
-            t.ifError(er, 'should exist');
-            t.ok(stat && stat.isDirectory(), 'should be directory');
-            t.end();
-        });
-    });
-});
+            t.ifError(er, 'should exist')
+            t.ok(stat && stat.isDirectory(), 'should be directory')
+            t.end()
+        })
+    })
+})
