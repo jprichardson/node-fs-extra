01b9ff7a9702cd31cad3e045e46e3ce3b05abf8c
JP Richardson <jprichardson@gmail.com>
2014-11-10 12:49:35 -0600

    test/mkdirp/clobber.test: removed semicolons

--- test/mkdirp/clobber.test.js
+++ test/mkdirp/clobber.test.js
@@ -1,24 +1,24 @@
-var mkdirp = require('../').mkdirp;
-var path = require('path');
-var fs = require('fs');
-var test = require('tap').test;
+var mkdirp = require('../').mkdirp
+var path = require('path')
+var fs = require('fs')
+var test = require('tap').test
 
-var ps = [ '', 'tmp' ];
+var ps = [ '', 'tmp' ]
 
 for (var i = 0; i < 25; i++) {
-  var dir = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
-  ps.push(dir);
+  var dir = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
+  ps.push(dir)
 }
 
-var file = ps.join('/');
+var file = ps.join('/')
 
 // a file in the way
-var itw = ps.slice(0, 3).join('/');
+var itw = ps.slice(0, 3).join('/')
 
 
 test('clobber-pre', function (t) {
   console.error("about to write to "+itw)
-  fs.writeFileSync(itw, 'I AM IN THE WAY, THE TRUTH, AND THE LIGHT.');
+  fs.writeFileSync(itw, 'I AM IN THE WAY, THE TRUTH, AND THE LIGHT.')
 
   fs.stat(itw, function (er, stat) {
     t.ifError(er)
@@ -28,10 +28,10 @@ test('clobber-pre', function (t) {
 })
 
 test('clobber', function (t) {
-  t.plan(2);
+  t.plan(2)
   mkdirp(file, 0755, function (err) {
-    t.ok(err);
-    t.equal(err.code, 'ENOTDIR');
-    t.end();
-  });
-});
+    t.ok(err)
+    t.equal(err.code, 'ENOTDIR')
+    t.end()
+  })
+})
