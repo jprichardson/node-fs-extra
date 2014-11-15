7ca7f5b874a45954b818cb9eb3112b08a6b62fd7
JP Richardson <jprichardson@gmail.com>
2014-11-15 14:01:01 -0600

    test/mkdirp/rel.test: 4 spaces to 2, removed semicolons, tap to assert

--- test/mkdirp/rel.test.js
+++ test/mkdirp/rel.test.js
@@ -1,30 +1,32 @@
-var mkdirp = require('../');
-var path = require('path');
-var fs = require('fs');
-var exists = fs.exists || path.exists;
-var test = require('tap').test;
+var assert = require('assert')
+var fs = require('fs')
+var path = require('path')
+var fse = require('../../')
+var testutil = require('testutil')
 
-test('rel', function (t) {
-    t.plan(5);
-    var x = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
-    var y = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
-    var z = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
+describe('mkdirp / rel', function() {
+  it('rel', function (done) {
+    var x = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
+    var y = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
+    var z = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
     
-    var cwd = process.cwd();
-    process.chdir('/tmp');
+    var cwd = process.cwd()
+    process.chdir(testutil.createTestDir('fs-extra'))
     
-    var file = [x,y,z].join('/');
+    var file = [x,y,z].join('/')
     
-    mkdirp(file, 0755, function (err) {
-        t.ifError(err);
-        exists(file, function (ex) {
-            t.ok(ex, 'file created');
-            fs.stat(file, function (err, stat) {
-                t.ifError(err);
-                process.chdir(cwd);
-                t.equal(stat.mode & 0777, 0755);
-                t.ok(stat.isDirectory(), 'target not a directory');
-            })
+    fse.mkdirp(file, 0755, function (err) {
+      assert.ifError(err)
+      fs.exists(file, function (ex) {
+        assert.ok(ex, 'file created')
+        fs.stat(file, function (err, stat) {
+          assert.ifError(err)
+          process.chdir(cwd)
+          assert.equal(stat.mode & 0777, 0755)
+          assert.ok(stat.isDirectory(), 'target not a directory')
+          done()
         })
-    });
-});
+      })
+    })
+  })
+})
