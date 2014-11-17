9417961f36eee8651ad6603deea4b7a07de6062a
JP Richardson <jprichardson@gmail.com>
2014-11-17 10:18:12 -0600

    test/mkdirp/return.test: 4 spaces to 2, removed semicolons, tap to assert

--- test/mkdirp/return.test.js
+++ test/mkdirp/return.test.js
@@ -1,25 +1,29 @@
-var mkdirp = require('../');
-var path = require('path');
-var fs = require('fs');
-var test = require('tap').test;
+var assert = require('assert')
+var fs = require('fs')
+var path = require('path')
+var fse = require('../../')
+var testutil = require('testutil')
 
-test('return value', function (t) {
-    t.plan(4);
-    var x = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
-    var y = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
-    var z = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
+describe('mkdirp / return value', function() {
+  it('should', function (done) {
+    var x = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
+    var y = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
+    var z = Math.floor(Math.random() * Math.pow(16,4)).toString(16)
 
-    var file = '/tmp/' + [x,y,z].join('/');
+    var dir =  testutil.createTestDir('fs-extra') + '/'
+    var file = dir + [x,y,z].join('/')
 
     // should return the first dir created.
     // By this point, it would be profoundly surprising if /tmp didn't
     // already exist, since every other test makes things in there.
-    mkdirp(file, function (err, made) {
-        t.ifError(err);
-        t.equal(made, '/tmp/' + x);
-        mkdirp(file, function (err, made) {
-          t.ifError(err);
-          t.equal(made, null);
-        });
-    });
-});
+    fse.mkdirp(file, function (err, made) {
+      assert.ifError(err)
+      assert.equal(made, dir + x)
+      fse.mkdirp(file, function (err, made) {
+        assert.ifError(err)
+        assert.equal(made, null)
+        done()
+      })
+    })
+  })
+})
