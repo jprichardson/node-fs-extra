666e0cca0c98641001eae03c2525a96ccb50d794
JP Richardson <jprichardson@gmail.com>
2015-04-04 23:40:44 -0500

    test/move/move-clobber: extracted out clobber test into here

---
+++ test/move/move-clobber.test.js
@@ -0,0 +1,63 @@
+var assert = require('assert')
+var fs = require('fs')
+var path = require('path')
+var os = require('os')
+var fse = require(process.cwd())
+
+/* global afterEach, beforeEach, describe, it */
+
+describe('move / clobber', function () {
+  var TEST_DIR, FIXTURES_DIR
+
+  beforeEach(function () {
+    TEST_DIR = path.join(os.tmpdir(), 'fs-extra', 'move')
+    fse.emptyDirSync(TEST_DIR)
+
+    FIXTURES_DIR = path.join(TEST_DIR, 'fixtures')
+    fse.copySync(path.join(__dirname, './fixtures'), FIXTURES_DIR)
+  })
+
+  afterEach(function (done) {
+    fse.remove(TEST_DIR, done)
+  })
+
+  describe('> when clobber = true', function () {
+    describe('> when dest is a directory', function () {
+      it('should clobber the destination', function (done) {
+        // use fixtures dir as dest since it has stuff
+        var dest = FIXTURES_DIR
+        var paths = fs.readdirSync(dest)
+
+        // verify dest has stuff
+        assert(paths.indexOf('a-file') >= 0)
+        assert(paths.indexOf('a-folder') >= 0)
+
+        // create new source dir
+        var src = path.join(TEST_DIR, 'src')
+        fse.ensureDirSync(src)
+        fse.mkdirsSync(path.join(src, 'some-folder'))
+        fs.writeFileSync(path.join(src, 'some-file'), 'hi')
+
+        // verify source has stuff
+        paths = fs.readdirSync(src)
+        assert(paths.indexOf('some-file') >= 0)
+        assert(paths.indexOf('some-folder') >= 0)
+
+        fse.move(src, dest, {clobber: true}, function (err) {
+          if (err) return done(err)
+
+          // verify dest does not have old stuff
+          var paths = fs.readdirSync(dest)
+          assert.strictEqual(paths.indexOf('a-file'), -1)
+          assert.strictEqual(paths.indexOf('a-folder'), -1)
+
+          // verify dest has new stuff
+          assert(paths.indexOf('some-file') >= 0)
+          assert(paths.indexOf('some-folder') >= 0)
+
+          done()
+        })
+      })
+    })
+  })
+})
--- test/move/move.test.js
+++ test/move/move.test.js
@@ -216,47 +216,5 @@ describe('move', function () {
         })
       })
     })
-
-    describe('> a folder with a bunch of stuff', function () {})
-
-    describe('> when clobber = true', function () {
-      describe('> when dest is a directory', function () {
-        it('should clobber the destination', function (done) {
-          // use fixtures dir as dest since it has stuff
-          var dest = FIXTURES_DIR
-          var paths = fs.readdirSync(dest)
-
-          // verify dest has stuff
-          assert(paths.indexOf('a-file') >= 0)
-          assert(paths.indexOf('a-folder') >= 0)
-
-          // create new source dir
-          var src = path.join(TEST_DIR, 'src')
-          fse.ensureDirSync(src)
-          fse.mkdirsSync(path.join(src, 'some-folder'))
-          fs.writeFileSync(path.join(src, 'some-file'), 'hi')
-
-          // verify source has stuff
-          paths = fs.readdirSync(src)
-          assert(paths.indexOf('some-file') >= 0)
-          assert(paths.indexOf('some-folder') >= 0)
-
-          fse.move(src, dest, {clobber: true}, function (err) {
-            if (err) return done(err)
-
-            // verify dest does not have old stuff
-            var paths = fs.readdirSync(dest)
-            assert.strictEqual(paths.indexOf('a-file'), -1)
-            assert.strictEqual(paths.indexOf('a-folder'), -1)
-
-            // verify dest has new stuff
-            assert(paths.indexOf('some-file') >= 0)
-            assert(paths.indexOf('some-folder') >= 0)
-
-            done()
-          })
-        })
-      })
-    })
   })
 })
