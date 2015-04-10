ae34eee0ca42f5a531b3abcfd8185a8c3c4e78e5
JP Richardson <jprichardson@gmail.com>
2015-04-10 04:16:25 -0500

    test/remove/remove-alias: created

--- test/remove/remote.test.js
+++ test/remove/remote.test.js
@@ -102,23 +102,4 @@ describe('remove', function () {
       fse.remove(file)
     })
   })
-
-  describe('+ delete()', function () {
-    it('should delete an empty directory', function (done) {
-      assert(fs.existsSync(TEST_DIR))
-      fse['delete'](TEST_DIR, function (err) {
-        assert.ifError(err)
-        assert(!fs.existsSync(TEST_DIR))
-        done()
-      })
-    })
-  })
-
-  describe('+ deleteSync()', function () {
-    it('should delete directories and files synchronously', function () {
-      assert(fs.existsSync(TEST_DIR))
-      fse.deleteSync(TEST_DIR)
-      assert(!fs.existsSync(TEST_DIR))
-    })
-  })
 })
