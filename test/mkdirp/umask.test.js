7c51598f5f9cb9b39faefeec1ebb563257f30de3
JP Richardson <jprichardson@gmail.com>
2015-04-10 03:50:45 -0500

    test/mkdirp/umask: rename oct

--- test/mkdirp/umask.test.js
+++ test/mkdirp/umask.test.js
@@ -6,7 +6,7 @@ var fse = require('../../')
 
 /* global afterEach, beforeEach, describe, it */
 
-var oct777 = parseInt('777', 8)
+var o777 = parseInt('777', 8)
 
 describe('mkdirp', function () {
   var TEST_DIR
@@ -43,7 +43,7 @@ describe('mkdirp', function () {
             assert.ok(ex, 'file created')
             fs.stat(_rndDir, function (err, stat) {
               assert.ifError(err)
-              assert.equal(stat.mode & oct777, oct777 & (~process.umask()))
+              assert.equal(stat.mode & o777, o777 & (~process.umask()))
               assert.ok(stat.isDirectory(), 'target not a directory')
               done()
             })
@@ -66,7 +66,7 @@ describe('mkdirp', function () {
           assert.ok(ex, 'file created')
           fs.stat(_rndDir, function (err, stat) {
             assert.ifError(err)
-            assert.equal(stat.mode & oct777, (oct777 & (~process.umask())))
+            assert.equal(stat.mode & o777, (o777 & (~process.umask())))
             assert.ok(stat.isDirectory(), 'target not a directory')
             done()
           })
