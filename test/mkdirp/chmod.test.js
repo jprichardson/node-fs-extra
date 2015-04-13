49a0bef0379106bec3a93183f7830e6059039895
JP Richardson <jprichardson@gmail.com>
2015-04-12 22:15:08 -0500

    removed testutil more

--- test/mkdirp/chmod.test.js
+++ test/mkdirp/chmod.test.js
@@ -1,8 +1,8 @@
 var assert = require('assert')
 var fs = require('fs')
 var path = require('path')
+var os = require('os')
 var fse = require('../../')
-var testutil = require('testutil')
 
 /* global afterEach, beforeEach, describe, it */
 
@@ -10,20 +10,23 @@ var o755 = parseInt('755', 8)
 var o744 = parseInt('744', 8)
 var o777 = parseInt('777', 8)
 
-var TEST_DIR = ''
-
-var ps = []
-for (var i = 0; i < 25; i++) {
-  var dir = Math.floor(Math.random() * Math.pow(16, 4)).toString(16)
-  ps.push(dir)
-}
-
-var file = ps.join('/')
-
 describe('mkdirp / chmod', function () {
-  beforeEach(function () {
-    TEST_DIR = testutil.createTestDir('fs-extra')
-    file = path.join(TEST_DIR, file)
+  var TEST_DIR
+  var TEST_SUBDIR
+
+  beforeEach(function (done) {
+    var ps = []
+    for (var i = 0; i < 15; i++) {
+      var dir = Math.floor(Math.random() * Math.pow(16, 4)).toString(16)
+      ps.push(dir)
+    }
+
+    TEST_SUBDIR = ps.join('/')
+
+    TEST_DIR = path.join(os.tmpdir(), 'fs-extra', 'mkdirp-chmod')
+    TEST_SUBDIR = path.join(TEST_DIR, TEST_SUBDIR)
+
+    fse.emptyDir(TEST_DIR, done)
   })
 
   afterEach(function (done) {
@@ -32,9 +35,9 @@ describe('mkdirp / chmod', function () {
 
   it('chmod-pre', function (done) {
     var mode = o744
-    fse.mkdirp(file, mode, function (er) {
+    fse.mkdirp(TEST_SUBDIR, mode, function (er) {
       assert.ifError(er, 'should not error')
-      fs.stat(file, function (er, stat) {
+      fs.stat(TEST_SUBDIR, function (er, stat) {
         assert.ifError(er, 'should exist')
         assert.ok(stat && stat.isDirectory(), 'should be directory')
         assert.equal(stat && stat.mode & o777, mode, 'should be 0744')
@@ -45,9 +48,9 @@ describe('mkdirp / chmod', function () {
 
   it('chmod', function (done) {
     var mode = o755
-    fse.mkdirp(file, mode, function (er) {
+    fse.mkdirp(TEST_SUBDIR, mode, function (er) {
       assert.ifError(er, 'should not error')
-      fs.stat(file, function (er, stat) {
+      fs.stat(TEST_SUBDIR, function (er, stat) {
         assert.ifError(er, 'should exist')
         assert.ok(stat && stat.isDirectory(), 'should be directory')
         done()
