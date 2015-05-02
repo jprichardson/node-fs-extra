1ccc31c8aa5ef62039346535c501960cb72c2869
KimJiTae <shine.kim@navercorp.com>
2015-05-02 11:50:26 +0900

    modify standard check

--- test/row2arr/read.test.js
+++ test/row2arr/read.test.js
@@ -20,46 +20,46 @@ describe('read', function () {
 
   describe('+ convert to array', function () {
     it('should read a file and convert row to arr', function (done) {
-      var text = "aaaa\nbbbb\ncccc\ndddd"
+      var text = 'aaaa\nbbbb\ncccc\ndddd'
 
       var file = path.join(TEST_DIR, 'file.txt')
       fs.writeFileSync(file, text)
 
       fse.readRow2Arr(file, function (err, arr) {
         assert.ifError(err)
-        assert.strictEqual(arr[0], "aaaa")
-        assert.strictEqual(arr[1], "bbbb")
-        assert.strictEqual(arr[2], "cccc")
+        assert.strictEqual(arr[0], 'aaaa')
+        assert.strictEqual(arr[1], 'bbbb')
+        assert.strictEqual(arr[2], 'cccc')
 
         done()
       })
     })
 
     it('should read a file and convert row to arr by sync', function () {
-      var text = "aaaa\nbbbb\ncccc\ndddd"
+      var text = 'aaaa\nbbbb\ncccc\ndddd'
 
       var file = path.join(TEST_DIR, 'file.txt')
       fs.writeFileSync(file, text)
 
       var rowarr = fse.readRow2ArrSync(file)
 
-      assert.strictEqual(rowarr[0], "aaaa")
-      assert.strictEqual(rowarr[1], "bbbb")
-      assert.strictEqual(rowarr[2], "cccc")
+      assert.strictEqual(rowarr[0], 'aaaa')
+      assert.strictEqual(rowarr[1], 'bbbb')
+      assert.strictEqual(rowarr[2], 'cccc')
     })
 
     it('should be ignore comment line', function () {
-      var text = "aaaa\nbbbb\ncccc\n# ignore line\ndddd"
+      var text = 'aaaa\nbbbb\ncccc\n# ignore line\ndddd'
 
       var file = path.join(TEST_DIR, 'file.txt')
       fs.writeFileSync(file, text)
 
       var rowarr = fse.readRow2ArrSync(file)
 
-      assert.strictEqual(rowarr[0], "aaaa")
-      assert.strictEqual(rowarr[1], "bbbb")
-      assert.strictEqual(rowarr[2], "cccc")
-      assert.strictEqual(rowarr[3], "dddd")
+      assert.strictEqual(rowarr[0], 'aaaa')
+      assert.strictEqual(rowarr[1], 'bbbb')
+      assert.strictEqual(rowarr[2], 'cccc')
+      assert.strictEqual(rowarr[3], 'dddd')
     })
   })
 })
