c989b6ea0e859dd1aadbc07076b7ca2d61bce546
JP Richardson <jprichardson@gmail.com>
2015-06-17 09:48:53 -0500

    JavaScript Standard Style

--- test.js
+++ test.js
@@ -4,23 +4,21 @@ var walk = require('./lib/walk/')
 var mocha = new Mocha({
   ui: 'bdd',
   reporter: 'spec',
-  timeout: 30000,
+  timeout: 30000
 })
 
 var filter = function (item, stat) {
   if (stat.isDirectory()) return true
-  return  item.lastIndexOf('.test.js') === (item.length - '.test.js'.length)
+  return item.lastIndexOf('.test.js') === (item.length - '.test.js'.length)
 }
 
-var walker = walk('./test', filter)
+walk('./test', filter)
   .on('data', function (item, stat) {
     if (!stat.isFile()) return
     mocha.addFile(item)
   })
   .on('end', function () {
-    mocha.run(function(failures){
-      process.exit(failures);
+    mocha.run(function (failures) {
+      process.exit(failures)
     })
   })
-
-
